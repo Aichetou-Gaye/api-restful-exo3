5b3817767d9c32741c0826bf34568b7a0101671d
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-10-08 09:30:09 +0100

    Setup authentification with api key

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
-import express from 'express'
-
-
-const app = express()
+import express from "express";
+const app = express();
 
 app.listen(3000, (req, res) => {
-    console.log("Connected with express");
-})
+  console.log("Connected with express");
+});
 
-app.get("/api/private-data", (req, res) => {
-    res.json({ message: "Hello world!"})
-})
+const genAPIKey = () => {
+  return [...Array(10)].map((e) => ((Math.random() * 10) | 0))
+  .join("");
+};
+
+const key = genAPIKey()
+console.log(key);
+
+
+const authenticateKey = (req, res, next) => {
+  let api_key = req.header("x-api-key");
+  if (api_key && api_key == key) {
+    next()
+  } else {
+    res.status(403).send({ error: { code: 403, message: "You are not allowed." } });
+  }
+};
+
+app.get("/api/private-data", authenticateKey, (req, res) => {
+    res.status(200).send({ success: { code: 200, message: "You are allowed." } });
+});
